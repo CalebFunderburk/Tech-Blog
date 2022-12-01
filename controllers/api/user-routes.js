de4230aa9cee4e72cf32e333e439a4786e53dcbf
Caleb <calebfunderburk@icloud.com>
2022-12-01 14:51:54 -0500

    added User routes for logging in & out

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -47,6 +47,47 @@ router.post('/', (req, res) => {
     })
 })
 
+// Route to login a user
+router.post('/login', (req, res) => {
+    User.findOne({
+      where: {
+        email: req.body.email
+      }
+    }).then(userData => {
+      if (!userData) {
+        res.status(400).json({ message: 'No user with that email address!' })
+        return
+      }
+  
+      const validPassword = userData.checkPassword(req.body.password)
+  
+      if (!validPassword) {
+        res.status(400).json({ message: 'Incorrect password!' })
+        return
+      }
+  
+      req.session.save(() => {
+        req.session.user_id = userData.id
+        req.session.username = userData.username
+        req.session.loggedIn = true
+  
+        res.json({ user: userData, message: 'You are now logged in!' })
+      })
+    })
+  })
+  
+  // Route to logout a user
+  router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+      req.session.destroy(() => {
+        res.status(204).end()
+      })
+    }
+    else {
+      res.status(404).end()
+    }
+  })
+
 // Update a user
 router.put('/:id', (req, res) => {
     User.update(req.body, {
