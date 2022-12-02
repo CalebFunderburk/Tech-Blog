90db9d4d75567af407c64b56c8e7b9f23be2cc76
Caleb <calebfunderburk@icloud.com>
2022-12-01 21:29:33 -0500

    set up views and homepage routes

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,15 @@
 // Imports
 const router = require('express').Router()
 const apiRoutes = require('./api')
+const homeRoutes = require('./home-routes')
 
 // Plugins
 router.use('/api', apiRoutes)
+router.use('/', homeRoutes)
+
+router.use((req, res) => {
+    res.status(404).end()
+})
 
 // Exports
 module.exports = router
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const session = require('express-session')
-// add handlebars here
+
+const path = require('path')
+const exphbs = require('express-handlebars')
 
 const app = express()
 const PORT = process.env.PORT || 3001
@@ -20,11 +22,14 @@ const sess = {
 
 app.use(session(sess))
 
-// helpers and view engine stuff for handlebars goes here
+const helpers = require('./utils/helpers')
+const hbs = exphbs.create({ helpers })
+app.engine('handlebars', hbs.engine)
+app.set('view engine', 'handlebars')
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-// app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use(require('./controllers'))
 
